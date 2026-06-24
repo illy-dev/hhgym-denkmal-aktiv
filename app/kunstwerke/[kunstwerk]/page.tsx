6855bceb6f9e51430182f2b74d5b573c796d6fad
Illy <125080991+illy-dev@users.noreply.github.com>
2026-06-24 21:44:51 +0200

    umbennenung zu original

--- app/kunstwerke/[kunstwerk]/page.tsx
+++ app/kunstwerke/[kunstwerk]/page.tsx
@@ -38,7 +38,7 @@ export default async function KunstwerkPage({ params }: PageProps) {
                 Neue Version
               </TabsTrigger>
               <TabsTrigger value="old" className="rounded-full px-4 py-2 data-[state=active]:bg-primary data-[state=active]:text-white">
-                Alte Version
+                Original
               </TabsTrigger>
             </TabsList>
             <TabsContent value="new" className="relative">
